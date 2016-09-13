c6daa1b8486009befb8748864bc37ca3c7ece6a0
mehtank <mehtank>
2016-09-12 18:34:58 -0700

    Sweep across lanes and number of cars

--- python/sweepcirc.py
+++ python/sweepcirc.py
@@ -1,45 +1,45 @@
 import subprocess
 import sys
 
-from makecirc import makecirc
+from makecirc import makecirc, makenet
 from parsexml import parsexml
 from plots import pcolor
 
 
 base="circsweep"
+length=1000
+maxspeed=30
 
-for numcars in (5, 10):
-    name = "%s-%02d" % (base, numcars)
+for numlanes in (1, 2, 3):
+    netfn = makenet("base", length=length, lanes=numlanes)
 
-    # set up simulation
-    print "Setting up simulaton..."
-    makecirc(name, numcars=numcars)
+    for numcars in (5, 10, 15, 20, 25, 30):
+        name = "%s-%dl-%02d" % (base, numlanes, numcars)
 
-    """
-    # run simulation
-    print "Running simulaton..."
-    retcode = subprocess.call(
-	['sumo', "-c", cfgfn, "--no-step-log"], 
-        stdout=sys.stdout, stderr=sys.stderr)
-    print(">> Simulation closed with status %s" % retcode)
-    sys.stdout.flush()
+        # set up simulation
+        print "Setting up simulaton..."
+        cfgfn, nsfn, atfn, lcfn = makecirc(name, netfn=netfn, numcars=numcars, maxspeed=maxspeed)
 
-    # TODO: Get values from .xml
-    edgestarts = {"bottom": 0, "right": 250, "top": 500, "left": 750}
-    maxpos = 1000
-    maxspeed = 30
+        # run simulation
+        print "Running simulaton..."
+        retcode = subprocess.call(
+            ['sumo', "-c", cfgfn, "--no-step-log"], 
+            stdout=sys.stdout, stderr=sys.stderr)
+        print(">> Simulation closed with status %s" % retcode)
+        sys.stdout.flush()
 
-    fn = "data/circulardump%02d.xml" % numcars
+        # TODO: Get values from .xml?
+        l4 = length/4.
+        edgestarts = {"bottom": 0, "right": l4, "top": 2*l4, "left": 3*l4}
 
-    print "Parsing xml file %s..." % fn
-    alldata, trng, xrng, speeds = parsexml(fn, edgestarts, maxpos)
+        print "Parsing xml file %s..." % nsfn
+        alldata, trng, xrng, speeds = parsexml(nsfn, edgestarts, length)
 
-    print "Generating interpolated plot..."
-    plt = pcolor("Traffic jams (interpolated data, %d cars)" % numcars, 
-                (xrng, "Position along loop (m)"),
-                (trng, "Time (s)"),
-                (speeds, 0, maxspeed, "Speed (m/s)"))
+        print "Generating interpolated plot..."
+        plt = pcolor("Traffic jams (interpolated data, %d cars)" % numcars, 
+                    (xrng, "Position along loop (m)"),
+                    (trng, "Time (s)"),
+                    (speeds, 0, maxspeed, "Speed (m/s)"))
 
-    plt.savefig('img/interp%02d.png' % numcars)
-    print "Done!"
-    """
+        plt.savefig('img/interp-%s.png' % name)
+        print "Done!"
