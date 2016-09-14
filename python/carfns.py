03b38322ff9f456daba713890b6f09f2f62be69d
Cathy Wu <cathywu@eecs.berkeley.edu>
2016-09-14 11:39:46 -0700

    Midpoint controller

--- python/carfns.py
+++ python/carfns.py
@@ -47,7 +47,7 @@ def changeFasterLaneBuilder(speedThreshold = 5, likelihood = 0.5,
             traci.vehicle.changeLane(car["id"], maxl, 10000)
     return carFn
 
-def ACCFnBuilder(follow_sec = 3.0, max_speed = 26.8, gain = 0.1):
+def ACCFnBuilder(follow_sec = 3.0, max_speed = 26.8, gain = 0.01):
     """
     Basic adaptive cruise control (ACC) controller
     :param follow_sec:
@@ -58,6 +58,7 @@ def ACCFnBuilder(follow_sec = 3.0, max_speed = 26.8, gain = 0.1):
 
     def ACCFn((idx, car), sim, step):
         """
+        P controller
         :param idx:
         :param car:
         :param sim:
@@ -90,12 +91,60 @@ def ACCFnBuilder(follow_sec = 3.0, max_speed = 26.8, gain = 0.1):
             # speed up
             new_speed = min(curr_speed + gain * delta, max_speed)
             traci.vehicle.slowDown(vehID, new_speed, 1000) # 2.5 sec
-            # print curr_speed, new_speed, front_speed, delta, front_dist, follow_dist
+            print step, "FASTER", curr_speed, new_speed, front_speed, delta, front_dist, follow_dist
         elif follow_dist > front_dist:
             # slow down
             new_speed = max(curr_speed + gain * delta, 0)
             traci.vehicle.slowDown(vehID, new_speed, 1000) # 2.5 sec
-        # print curr_speed, new_speed, front_speed, delta, front_dist, follow_dist
-        # print (front_vID, front_dist), (back_vID, back_dist)
+            print step, "SLOWER", curr_speed, new_speed, front_speed, delta, front_dist, follow_dist
 
     return ACCFn
+
+def MidpointFnBuilder(max_speed = 26.8, gain = 0.01):
+    """
+    Basic adaptive cruise control (ACC) controller
+    :param follow_sec:
+    :param max_speed: 26.8 m/s = 60 mph
+    :param gain:
+    :return: ACCFn to input to a carParams
+    """
+
+    def MidpointFn((idx, car), sim, step):
+        """
+        :param idx:
+        :param car:
+        :param sim:
+        :param step:
+        :return:
+        """
+        vehID = car["id"]
+
+        if step < 250:
+            return
+
+        try:
+            [back_car, front_car] = sim.getCars(idx, numBack=1, numForward=1, lane=car["lane"])
+        except ValueError:
+            # Not enough cars on lane
+            return
+
+        front_dist = (front_car["x"] - car["x"]) % sim.length
+        back_dist = (car["x"] - back_car["x"]) % sim.length
+
+        curr_speed = car["v"]
+        front_speed = front_car["v"]
+        follow_dist = (front_dist + back_dist)/2
+        delta = front_dist - follow_dist
+        # print delta, curr_speed, front_speed, curr_speed-front_speed
+        if follow_dist < front_dist and curr_speed < max_speed:
+            # speed up
+            new_speed = min(curr_speed + gain * delta, max_speed)
+            traci.vehicle.slowDown(vehID, new_speed, 1000) # 2.5 sec
+            print step, "FASTER", curr_speed, new_speed, front_speed, delta, front_dist, follow_dist
+        elif follow_dist > front_dist:
+            # slow down
+            new_speed = max(curr_speed + gain * delta, 0)
+            traci.vehicle.slowDown(vehID, new_speed, 1000) # 2.5 sec
+            print step, "SLOWER", curr_speed, new_speed, front_speed, delta, front_dist, follow_dist
+
+    return MidpointFn
