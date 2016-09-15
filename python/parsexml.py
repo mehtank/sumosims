30ab2726c739fdadca88fa4182fb13852a2fea30
mehtank <mehtank>
2016-09-15 00:08:07 -0700

    Colored occupancy plots

--- python/parsexml.py
+++ python/parsexml.py
@@ -31,6 +31,7 @@ def parsexml(fn, edgestarts, xmax, vdefault=0):
     laneoccupancy = {}
     avgspeeds = {}
     totfuel = {}
+    typecolors = {}
 
     for timestep in obj.timestep:
         t = float(timestep.get("time"))
@@ -40,6 +41,7 @@ def parsexml(fn, edgestarts, xmax, vdefault=0):
             for vehicle in timestep.vehicle:
                 d = {}
                 d["name"] = vehicle.get("id")
+                d["type"] = vehicle.get("id")[:-4]
                 d["edge"] = vehicle.get("lane")[:-2]
                 d["v"] = float(vehicle.get("speed"))
                 d["pos"] = float(vehicle.get("pos"))
@@ -56,12 +58,17 @@ def parsexml(fn, edgestarts, xmax, vdefault=0):
 
         for lid, thislane in lanedata.iteritems():
             f = interp([x["x"] for x in thislane], [x["v"] for x in thislane], xmax, vdefault)
-            intx = [int(x["x"]) for x in thislane]
+            types = set([x["type"] for x in thislane])
+            for tp in types:
+                if tp not in typecolors:
+                    typecolors[tp] = len(typecolors)+1
+
+            intx = dict((int(x["x"]), typecolors[x["type"]]) for x in thislane)
             fuel = [x["fuel"] for x in thislane]
 
             fx = f(xrng)
             lanespeeds.setdefault(lid, [[vdefault]*len(xrng)]*len(trng)).append(fx)
-            laneoccupancy.setdefault(lid, [[0]*len(xrng)]*len(trng)).append([1 if x in intx else 0 for x in xrng])
+            laneoccupancy.setdefault(lid, [[0]*len(xrng)]*len(trng)).append([intx.get(x, 0) for x in xrng])
 
             dx = np.diff(np.array(xrng + [xrng[0] + xmax]))
             dt = dx * 1./fx
@@ -70,6 +77,4 @@ def parsexml(fn, edgestarts, xmax, vdefault=0):
 
         trng.append(t)
 
-    for x in avgspeeds.values():
-        print len(x)
     return trng, xrng, avgspeeds, lanespeeds, laneoccupancy, totfuel
