915e27763216c601f430e8edfb06505af70b6fac
mehtank <mehtank>
2016-09-15 21:35:33 -0700

    Dunno made some changes that are long gone

--- python/sugiyama_jam.py
+++ python/sugiyama_jam.py
@@ -1,4 +1,5 @@
 import random
+import itertools
 
 import config as defaults
 from loopsim import LoopSim
@@ -10,32 +11,61 @@ from agent_types import basicHumanParams as humanParams, \
 
 
 if __name__ == "__main__":
-    numLanes = 1
-    vTypeParams = humanParams
-    vTypeParams["tau"] = 0.5
-    vTypeParams["sigma"] = 0.5
-    vTypeParams["speedDev"] = 0
-    vTypeParams["speedFactor"] = 1
-
-    # vTypeParams = fillGapMidpointParams
-    # vTypeParams = gapFillerParams
-    # vTypeParams = midpointParams
-    # vTypeParams = IDMParams
-    # vTypeParams = ACCParams
-
-    if defaults.RANDOM_SEED:
-        print "Setting random seed to ", defaults.RANDOM_SEED
-        random.seed(defaults.RANDOM_SEED)
-
-    vTypeParams["count"] = 22 * numLanes
-
-    opts = {
-        "paramsList" : [vTypeParams],
-        "simSteps"   : 500,
-        "tag"        : "SugiyamaJam",
-    }
+    numLanes = 2
 
     sim = LoopSim("loopsim", length=230, numLanes=numLanes, simStepLength=0.5)
 
-    sim.simulate(opts)
-    sim.plot(show=True, save=True)
+    vTypeParams = humanParams
+    vTypeParams["laneSpread"] = True
+    vTypeParams["tau"] = 0.5
+    vTypeParams["speedDev"] = 0.0
+    vTypeParams["speedFactor"] = 1
+
+    mParams = midpointParams
+    mParams["laneSpread"] = True
+
+    gParams = fillGapMidpointParams
+    gParams["laneSpread"] = True
+
+    for sigma in (0.5, 0.9):
+        vTypeParams["sigma"] = sigma
+
+        vTypeParams["count"] = 22
+        if defaults.RANDOM_SEED:
+            print "Setting random seed to ", defaults.RANDOM_SEED
+            random.seed(defaults.RANDOM_SEED)
+
+        for run in range(3):
+            opts = {
+                "paramsList" : [vTypeParams],
+                "simSteps"   : 500,
+                "tag"        : "Sugiyama-sigma=%03f-run=%d" % (sigma, run),
+            }
+
+            sim.simulate(opts)
+            sim.plot(show=False, save=True, speedRange=(0,30), fuelRange=(0, 40)).close("all")
+
+        for numRobots, rParams in itertools.product(\
+                (1, 5, 11, 22), \
+                (mParams, gParams)):
+
+            vTypeParams["count"] = (22-numRobots) 
+            rParams["count"] = numRobots 
+
+            if defaults.RANDOM_SEED:
+                print ">>> Setting random seed to ", defaults.RANDOM_SEED
+                random.seed(defaults.RANDOM_SEED)
+
+            for run in range(3):
+                opts = {
+                    "paramsList" : [vTypeParams, rParams],
+                    "simSteps"   : 500,
+                    "tag"        : "Sugiyama-sigma=%03f-run=%d" % (sigma, run),
+                }
+
+                print "***"
+                print "sigma =", sigma, "numRobots = ", numRobots, 
+                print "robot type =", rParams["name"], "run = ", run
+                print "***"
+                sim.simulate(opts)
+                sim.plot(show=False, save=True, speedRange=(0,30), fuelRange=(0, 40)).close("all")
