1d6f900f6aec03dd4dcabe7e209140a07c9348f5
mehtank <mehtank>
2016-09-16 09:55:05 -0700

    Additional config defaults

--- python/config.py
+++ python/config.py
@@ -1,15 +1,17 @@
 # random seed
 RANDOM_SEED = 23434
 
+# Number of waypoinst around circular curve
+RESOLUTION = 20
+
+# the name of the sumo binary
+BINARY = 'sumo'
+
 # the port used for communicating with your sumo instance
 PORT = 8873
 
 # the timestep used for simulation (s)
 SIM_STEP_LENGTH = 1.0
-
-LENGTH = 1000
-L4 = LENGTH/4.
-EDGESTARTS = {"bottom": 0, "right": L4, "top": 2*L4, "left": 3*L4}
 
 NET_PATH = "net/"
 IMG_PATH = "img/"
@@ -17,7 +19,9 @@ DATA_PATH = "data/"
 
 # Roadway speed limit
 # 30 m/s = 67.1081 mph
+# 35 m/s = 78.2928 mph
 SPEED_LIMIT = 35
 
 # Maximum speed for agents
+# 40 m/s = 89.4775 mph
 MAX_SPEED = 40
